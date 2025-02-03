61c627c8cc7d958fba216740fad33e0363d0d71c
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2025-02-03 13:58:31 +0200

    utils/solvers

--- src/shaders/libraries/utils/solvers/quadratic_solver.glsl
+++ src/shaders/libraries/utils/solvers/quadratic_solver.glsl
@@ -110,6 +110,53 @@ vec2 quadratic_solver(in vec3 coeffs, in float value, in float default_root)
     
 }
 
+// STRICTLY QUADRATIC SOLVERS
+vec2 _quadratic_solver(in vec3 coeffs, in float value)
+{
+    // set default roots
+    vec2 default_roots = vec2(-1.0);
+
+    // normalize equation coeffs.z * t^2 + coeffs.y * t + (coeffs.x - value) = 0
+    coeffs.x -= value;
+
+    // compute normalized quadratic coefficients 
+    vec2 quadratic_coeffs = coeffs.xy / coeffs.z;
+    quadratic_coeffs.y /= 2.0;
+
+    // compute quadratic discriminant
+    float quadratic_discriminant = quadratic_coeffs.y * quadratic_coeffs.y - quadratic_coeffs.x;
+    float sqrt_quadratic_discriminant = sqrt(abs(quadratic_discriminant));
+
+    // compute quadratic roots 
+    vec2 quadratic_roots = - quadratic_coeffs.y + vec2(-1.0, 1.0) * sqrt_quadratic_discriminant;
+   
+    // quadratic solutions
+    return (quadratic_discriminant < 0.0) ? default_roots : quadratic_roots;
+}
+
+vec2 _quadratic_solver(in vec3 coeffs, in float value, in float flag)
+{
+    // set default roots
+    vec2 default_roots = vec2(flag);
+
+    // normalize equation coeffs.z * t^2 + coeffs.y * t + (coeffs.x - value) = 0
+    coeffs.x -= value;
+
+    // compute normalized quadratic coefficients 
+    vec2 quadratic_coeffs = coeffs.xy / coeffs.z;
+    quadratic_coeffs.y /= 2.0;
+
+    // compute quadratic discriminant
+    float quadratic_discriminant = quadratic_coeffs.y * quadratic_coeffs.y - quadratic_coeffs.x;
+    float sqrt_quadratic_discriminant = sqrt(abs(quadratic_discriminant));
+
+    // compute quadratic roots 
+    vec2 quadratic_roots = - quadratic_coeffs.y + vec2(-1.0, 1.0) * sqrt_quadratic_discriminant;
+
+    // quadratic solutions
+    return (quadratic_discriminant < 0.0) ? default_roots : quadratic_roots;
+}
+
 #endif
 
 
