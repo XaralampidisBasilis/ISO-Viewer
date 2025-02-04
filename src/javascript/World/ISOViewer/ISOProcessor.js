bec77ba81b9cac8c7adb23d57a71b21013db4fec
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2025-02-04 17:00:36 +0200

    start async operations in ISOProcessor to avoid tensorflow crashing

--- src/javascript/World/ISOViewer/ISOProcessor.js
+++ src/javascript/World/ISOViewer/ISOProcessor.js
@@ -100,123 +100,98 @@ export default class ISOProcessor extends EventEmitter
       
     //  Computes
 
-    async computeIntensityMap()
+    async generateIntensityMap()
     {
-        if (this.computes.intensityMap.tensor instanceof tf.Tensor) 
-        {
-            tf.dispose(this.computes.intensityMap.tensor)
-        }
-
-        timeit('computeIntensityMap', () =>
-        {
-            this.computes.intensityMap.tensor = tf.tensor4d(this.volume.data, this.volume.parameters.shape,'float32')                
-            this.computes.intensityMap.parameters = {...this.volume.parameters}
-        })
-
+        this.computes.intensityMap.tensor = tf.tensor4d(this.volume.data, this.volume.parameters.shape,'float32')                
+        this.computes.intensityMap.parameters = {...this.volume.parameters}
+        
         // console.log(this.computes.intensityMap.parameters, /*this.computes.intensityMap.tensor.dataSync()*/)
     }
 
-    async computeOccupancyMap(threshold, subDivision)
+    async generateOccupancyMap(threshold, subDivision)
     {
         if (!(this.computes.intensityMap.tensor instanceof tf.Tensor)) 
         {
             throw new Error(`computeOccupancyMap: intensityMap is not computed`)
         }
+        const occupancyMap = await this.computeOccupancyMap(this.computes.intensityMap.tensor, threshold, subDivision)
+        const parameters = {}
 
-        if (this.computes.occupancyMap.tensor instanceof tf.Tensor) 
-        {
-            tf.dispose(this.computes.occupancyMap.tensor)
-        }
+        parameters.shape = occupancyMap.shape
+        parameters.threshold = threshold
+        parameters.subDivision = subDivision
+        parameters.invSubDivision = 1/subDivision
+        parameters.dimensions = new THREE.Vector3().fromArray(occupancyMap.shape.slice(0, 3).toReversed())
+        parameters.spacing = new THREE.Vector3().copy(this.volume.parameters.spacing).multiplyScalar(subDivision)
+        parameters.size = new THREE.Vector3().copy(parameters.dimensions).multiply(parameters.spacing)
+        parameters.numBlocks = parameters.dimensions.toArray().reduce((numBlocks, dimension) => numBlocks * dimension, 1)
+        parameters.invDimensions = new THREE.Vector3().fromArray(parameters.dimensions.toArray().map(x => 1/x))
+        parameters.invSpacing = new THREE.Vector3().fromArray(parameters.spacing.toArray().map(x => 1/x))
+        parameters.invSize = new THREE.Vector3().fromArray(parameters.size.toArray().map(x => 1/x))
 
-        timeit('computeOccupancyMap', () =>
-        {
-            const occupancyMap = this._computeOccupancyMap(this.computes.intensityMap.tensor, threshold, subDivision)
-            const parameters = {}
-
-            parameters.shape = occupancyMap.shape
-            parameters.threshold = threshold
-            parameters.subDivision = subDivision
-            parameters.invSubDivision = 1/subDivision
-            parameters.dimensions = new THREE.Vector3().fromArray(occupancyMap.shape.slice(0, 3).toReversed())
-            parameters.spacing = new THREE.Vector3().copy(this.volume.parameters.spacing).multiplyScalar(subDivision)
-            parameters.size = new THREE.Vector3().copy(parameters.dimensions).multiply(parameters.spacing)
-            parameters.numBlocks = parameters.dimensions.toArray().reduce((numBlocks, dimension) => numBlocks * dimension, 1)
-            parameters.invDimensions = new THREE.Vector3().fromArray(parameters.dimensions.toArray().map(x => 1/x))
-            parameters.invSpacing = new THREE.Vector3().fromArray(parameters.spacing.toArray().map(x => 1/x))
-            parameters.invSize = new THREE.Vector3().fromArray(parameters.size.toArray().map(x => 1/x))
-
-            this.computes.occupancyMap.tensor = occupancyMap
-            this.computes.occupancyMap.parameters = parameters
-        })
+        this.computes.occupancyMap.tensor = occupancyMap
+        this.computes.occupancyMap.parameters = parameters
 
         // console.log(this.computes.occupancyMap.parameters, /*this.computes.occupancyMap.tensor.dataSync()*/)
     }
 
-    async computeDistanceMap(maxIters)
+    async generateDistanceMap(maxIters)
     {
         if (!(this.computes.occupancyMap.tensor instanceof tf.Tensor)) 
         {
             throw new Error(`computeDistanceMap: occupancyMap is not computed`)
         }
 
-        if (this.computes.distanceMap.tensor instanceof tf.Tensor) 
-        {
-            tf.dispose(this.computes.distanceMap.tensor)
-        }
-       
-        timeit('computeDistanceMap', () =>
-        {
-            const distanceMap = this._computeDistanceMap(this.computes.occupancyMap.tensor, maxIters)
-            const parameters = {...this.computes.occupancyMap.parameters}
-            
-            const maxTensor = distanceMap.max()
-            parameters.maxDistance = maxTensor.arraySync()  
-            tf.dispose(maxTensor)
+        const distanceMap = await this.computeDistanceMap(this.computes.occupancyMap.tensor, maxIters)
+        const parameters = {...this.computes.occupancyMap.parameters}
+        const maxTensor = distanceMap.max()
+        const meanTensor = distanceMap.mean()
 
-            this.computes.distanceMap.tensor = distanceMap
-            this.computes.distanceMap.parameters = parameters
-        })
+        parameters.maxDistance = maxTensor.arraySync()  
+        parameters.meanTensor = meanTensor.arraySync()  
+        tf.dispose([maxTensor, meanTensor])
+
+        this.computes.distanceMap.tensor = distanceMap
+        this.computes.distanceMap.parameters = parameters
+    
 
         // console.log(this.computes.distanceMap.parameters, /*this.computes.distanceMap.tensor.dataSync()*/)
     }
 
-    async computeBoundingBox()
+    async generateBoundingBox()
     {
         if (!(this.computes.occupancyMap.tensor instanceof tf.Tensor)) 
         {
             throw new Error(`computeBoundingBox: occupancyMap is not computed`)
         }
+   
+        const boundingBox = await this.computeBoundingBox(this.computes.occupancyMap.tensor)
+        const parameters = {}
+        
+        parameters.minPosition = new THREE.Vector3().fromArray(boundingBox.minCoords).addScalar(0).multiply(this.computes.occupancyMap.parameters.spacing)
+        parameters.maxPosition = new THREE.Vector3().fromArray(boundingBox.maxCoords).addScalar(1).multiply(this.computes.occupancyMap.parameters.spacing)
+        parameters.minPosition.clamp(new THREE.Vector3(), this.volume.parameters.size)
+        parameters.maxPosition.clamp(new THREE.Vector3(), this.volume.parameters.size)
 
-        timeit('computeBoundingBox', () =>
-        {
-            const boundingBox = this._computeBoundingBox(this.computes.occupancyMap.tensor)
-            const parameters = {}
-            
-            parameters.minPosition = new THREE.Vector3().fromArray(boundingBox.minCoords).addScalar(0).multiply(this.computes.occupancyMap.parameters.spacing)
-            parameters.maxPosition = new THREE.Vector3().fromArray(boundingBox.maxCoords).addScalar(1).multiply(this.computes.occupancyMap.parameters.spacing)
-            parameters.minPosition.clamp(new THREE.Vector3(), this.volume.parameters.size)
-            parameters.maxPosition.clamp(new THREE.Vector3(), this.volume.parameters.size)
+        parameters.minCoords = new THREE.Vector3().copy(parameters.minPosition).divide(this.volume.parameters.spacing).addScalar(0.5).floor() // included in bbox
+        parameters.maxCoords = new THREE.Vector3().copy(parameters.maxPosition).divide(this.volume.parameters.spacing).subScalar(0.5).floor() // included in bbox
+        parameters.minCoords.clamp(new THREE.Vector3(), this.volume.parameters.dimensions)
+        parameters.maxCoords.clamp(new THREE.Vector3(), this.volume.parameters.dimensions)
 
-            parameters.minCoords = new THREE.Vector3().copy(parameters.minPosition).divide(this.volume.parameters.spacing).addScalar(0.5).floor() // included in bbox
-            parameters.maxCoords = new THREE.Vector3().copy(parameters.maxPosition).divide(this.volume.parameters.spacing).subScalar(0.5).floor() // included in bbox
-            parameters.minCoords.clamp(new THREE.Vector3(), this.volume.parameters.dimensions)
-            parameters.maxCoords.clamp(new THREE.Vector3(), this.volume.parameters.dimensions)
+        parameters.dimensions = new THREE.Vector3().subVectors(parameters.maxCoords, parameters.minCoords).addScalar(1)
+        parameters.numCells = parameters.dimensions.toArray().reduce((cells, dim) => cells * dim, 1)
+        parameters.numBlocks = parameters.dimensions.clone().divideScalar(this.computes.occupancyMap.parameters.subDivision).ceil().toArray().reduce((blocks, dim) => blocks * dim, 1)
+        parameters.maxCellCount = parameters.dimensions.toArray().reduce((intersections, cells) => intersections + cells, -2)
+        parameters.maxBlockCount = parameters.dimensions.clone().divideScalar(this.computes.occupancyMap.parameters.subDivision).ceil().toArray().reduce((intersections, blocks) => intersections + blocks, -2)
 
-            parameters.dimensions = new THREE.Vector3().subVectors(parameters.maxCoords, parameters.minCoords).addScalar(1)
-            parameters.numCells = parameters.dimensions.toArray().reduce((cells, dim) => cells * dim, 1)
-            parameters.numBlocks = parameters.dimensions.clone().divideScalar(this.computes.occupancyMap.parameters.subDivision).ceil().toArray().reduce((blocks, dim) => blocks * dim, 1)
-            parameters.maxCellCount = parameters.dimensions.toArray().reduce((intersections, cells) => intersections + cells, -2)
-            parameters.maxBlockCount = parameters.dimensions.clone().divideScalar(this.computes.occupancyMap.parameters.subDivision).ceil().toArray().reduce((intersections, blocks) => intersections + blocks, -2)
-
-            this.computes.boundingBox.parameters = parameters
-        })
+        this.computes.boundingBox.parameters = parameters
 
         // console.log(this.computes.boundingBox.parameters)
     }
     
     // Helpers
     
-    _computeOccupancyMap(intensityMap, threshold, subDivision) 
+    async computeOccupancyMap(intensityMap, threshold, division) 
     {
         // Scalars for threshold and output scaling
         const scalarThreshold = tf.scalar(threshold, 'float32')
@@ -225,13 +200,14 @@ export default class ISOProcessor extends EventEmitter
         const tensorPadded = tf.mirrorPad(intensityMap, [[1, 1], [1, 1], [1, 1], [0, 0]], 'symmetric')
 
         // Min pooling for lower bound detection
-        const minima = this._minPool3d(tensorPadded, [2, 2, 2], [1, 1, 1], 'valid')
+        const minima = this.minPool3d(tensorPadded, [2, 2, 2], [1, 1, 1], 'valid')
         const lesser = tf.lessEqual(minima, scalarThreshold)
         tf.dispose(minima)
 
         // Max pooling for upper bound detection
         const maxima = tf.maxPool3d(tensorPadded, [2, 2, 2], [1, 1, 1], 'valid')
         tf.dispose(tensorPadded)
+
         const greater = tf.greaterEqual(maxima, scalarThreshold)
         tf.dispose(scalarThreshold)
         tf.dispose(maxima)
@@ -241,16 +217,12 @@ export default class ISOProcessor extends EventEmitter
         tf.dispose(lesser)
         tf.dispose(greater)
 
-        // If no subdivision is needed, scale and return
-        if (subDivision === 1) 
-        {        
-            return occupied
-        }
-
         // Calculate necessary padding for valid subdivisions
-        const padAmounts = occupied.shape.map((dim, i) => {
-            const paddedDim = i < 3 ? Math.ceil(dim / subDivision) * subDivision : dim // Only pad spatial dimensions
-            return [0, paddedDim - dim]
+        const roundUp = (X, N) => N * Math.ceil(X / N)
+        const padAmounts = occupied.shape.map((dimension, i) => 
+        {
+            const paddedDim = (i < 3) ? roundUp(dimension, division) : dimension // Only pad spatial dimensions
+            return [0, paddedDim - dimension]
         })
 
         // Apply padding
@@ -258,38 +230,34 @@ export default class ISOProcessor extends EventEmitter
         tf.dispose(occupied)
 
         // Apply max pooling with valid padding and subdivision
-        const subDivisions = [subDivision, subDivision, subDivision]
-        const occupancyMap = tf.maxPool3d(occupiedPadded, subDivisions, subDivisions, 'valid')
+        const divisions = [division, division, division]
+        const occupancyMap = tf.maxPool3d(occupiedPadded, divisions, divisions, 'valid')
         tf.dispose(occupiedPadded)
 
         return occupancyMap
     }
 
-    _computeDistanceMap(occupancyMap, maxIters) 
+    async computeDistanceMap(occupancyMap, maxIters) 
     {
-        // Initialize next diffusion
-        let diffusionNext = tf.clone(occupancyMap)
-
-        // Initialize previous diffusion
+        // Initialize previous/next diffusion
         let diffusionPrev = tf.zeros(occupancyMap.shape, 'bool')
+        let diffusionNext = tf.clone(occupancyMap)
 
         // Initialize distance map 
         let distanceMap = tf.zeros(occupancyMap.shape, 'int32')
 
-        for (let iter = 0; iter <= maxIters; iter++) 
+        for (let i = 0; i <= maxIters; i++) 
         {
-            const scalarIter = tf.scalar(iter, 'int32')
+            const scalarIter = tf.scalar(i, 'int32')
 
             // Compute distance update
             const diffusionUpdate = tf.notEqual(diffusionNext, diffusionPrev)
             const distanceUpdate = diffusionUpdate.mul(scalarIter)
-            tf.dispose(diffusionUpdate)
-            tf.dispose( scalarIter)
+            tf.dispose([diffusionUpdate, scalarIter])
 
             // Update distance map
             const distanceMapTemp = distanceMap.add(distanceUpdate)
-            tf.dispose(distanceUpdate)
-            tf.dispose(distanceMap)
+            tf.dispose([distanceMap, distanceUpdate])
             distanceMap = distanceMapTemp
 
             // Update previous diffusion 
@@ -297,35 +265,35 @@ export default class ISOProcessor extends EventEmitter
             diffusionPrev = diffusionNext.clone()
 
             // Compute next diffusion with max pooling
-            const diffusionNextTemp = tf.maxPool3d(diffusionPrev, [3, 3, 3], [1, 1, 1], 'same')
             tf.dispose(diffusionNext)
-            diffusionNext = diffusionNextTemp
+            diffusionNext = tf.maxPool3d(diffusionPrev, [3, 3, 3], [1, 1, 1], 'same')
+
+            // Await for garbage disposal
+            await tf.nextFrame()
         }
-        tf.dispose(diffusionNext)
 
         // Compute final distance update
-        const scalarMaxIters = tf.scalar(maxIters, 'int32')
+        const scalarMax = tf.scalar(maxIters, 'int32')
         const diffusionUpdate = tf.logicalNot(diffusionPrev)
-        tf.dispose(diffusionPrev)
-        const distanceUpdate = diffusionUpdate.mul(scalarMaxIters)
-        tf.dispose(diffusionUpdate)
-        tf.dispose(scalarMaxIters)
+        const distanceUpdate = diffusionUpdate.mul(scalarMax)
+        tf.dispose([diffusionNext, diffusionPrev, diffusionUpdate, scalarMax])
+        await tf.nextFrame()
 
         // Update final distance map
         const distanceMapTemp = distanceMap.add(distanceUpdate)
-        tf.dispose(distanceUpdate)
-        tf.dispose(distanceMap)
+        tf.dispose([distanceMap, distanceUpdate])
         distanceMap = distanceMapTemp
+        await tf.nextFrame()
 
         // Return the final distance map
         return distanceMap
     }
 
-    _computeBoundingBox(occupancyMap) 
+    async computeBoundingBox(occupancyMap) 
     {
         // Compute the bounds for each axis dynamically
         const rank = occupancyMap.rank
-        const boundingIntervals = Array.from({ length: rank }, (_, axis) => this._argBounds(occupancyMap, axis))
+        const boundingIntervals = Array.from({ length: rank }, (_, axis) => this.argBounds(occupancyMap, axis))
 
         // Separate min and max bounds
         const minCoords = tf.stack(boundingIntervals.map(interval => interval[0]), 0)
@@ -342,7 +310,7 @@ export default class ISOProcessor extends EventEmitter
     
     }
 
-    _minPool3d(tensor4d, filterSize, strides, pad)
+    minPool3d(tensor4d, filterSize, strides, pad)
     {
         const scalarNegativeOne = tf.scalar(-1, 'float32')
         const negative = tensor4d.mul(scalarNegativeOne)
@@ -354,7 +322,7 @@ export default class ISOProcessor extends EventEmitter
         return tensorMinPool
     } 
 
-    _argBounds(occupancyMapBool, axis) 
+    argBounds(occupancyMapBool, axis) 
     {
         // Check input is boolean
         if (occupancyMapBool.dtype !== "bool") {
@@ -398,7 +366,7 @@ export default class ISOProcessor extends EventEmitter
         return [minInd, maxInd] 
     }
 
-    _quantize(tensor4d) 
+    quantize(tensor4d) 
     {
         return tf.tidy(() => 
         {
