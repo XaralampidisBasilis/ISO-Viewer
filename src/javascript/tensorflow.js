f2b281b8f5a21c031601024832d8f503ab02a924
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2025-10-07 15:12:01 +0300

    minor

--- src/javascript/tensorflow.js
+++ src/javascript/tensorflow.js
@@ -1,16 +1,19 @@
 import * as tf from '@tensorflow/tfjs'
 
-console.time('setTensorflow') 
+export async function setTensorflow() 
+{
+    console.time('setTensorflow')
 
-tf.enableProdMode() 
-tf.env().set('WEBGL_FORCE_F16_TEXTURES', false) // halves bandwidth on many GPUs
-tf.env().set('WEBGL_PACK', true)               // ensure packing is on
-tf.env().set('WEBGL_CPU_FORWARD', false)       // be strict about staying on GPU
-tf.env().set('WEBGL_VERSION', 2)               // prefer WebGL2 if available
-tf.env().set('WEBGL_DELETE_TEXTURE_THRESHOLD', 0)
-tf.env().set('WEBGL_FLUSH_THRESHOLD', 1)
+    tf.enableProdMode()
+    tf.env().set('WEBGL_FORCE_F16_TEXTURES', false)
+    tf.env().set('WEBGL_PACK', true)
+    tf.env().set('WEBGL_CPU_FORWARD', false)
+    tf.env().set('WEBGL_VERSION', 2)
+    tf.env().set('WEBGL_DELETE_TEXTURE_THRESHOLD', 0)
+    tf.env().set('WEBGL_FLUSH_THRESHOLD', 1)
 
-await tf.setBackend('webgl')
-await tf.ready()
+    await tf.setBackend('webgl')
+    await tf.ready()
 
-console.timeEnd('setTensorflow') 
+    console.timeEnd('setTensorflow')
+}
--- src/script.js
+++ src/script.js
@@ -1,12 +1,15 @@
 
-import './javascript/tensorflow'
+import { setTensorflow } from './javascript/tensorflow';
 import Experience from './javascript/Experience'
 
-const canvas = document.querySelector('canvas.webgl')
-const context = canvas.getContext('webgl2')
-if (!context) 
+(async () => 
 {
-    throw new Error('WebGL2 not supported by your browser or device.')
-}
+    await setTensorflow()
 
-const experience = new Experience(canvas, context) 
+    const canvas = document.querySelector('canvas.webgl')
+    const context = canvas.getContext('webgl2')
+    if (!context) throw new Error('WebGL2 not supported by your browser or device.')
+
+    const experience = new Experience(canvas, context) 
+}
+)()
