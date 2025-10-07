e255dcfb729e340111b4daf24eabe50fbc328d9c
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2025-10-07 15:15:37 +0300

    deletions

--- src/javascript/sources.js
+++ src/javascript/sources.js
@@ -5,9 +5,7 @@ export default [
         name: 'volume',
         type: 'niftiFile',
         // path: 'nifti/colon/volume.nii.gz',
-        // path: 'nifti/cardiac/mr_train_1001_image.nii.gz',
         // path: 'nifti/cardiac/mr_train_1014_image.nii.gz',
-        // path: 'nifti/cardiac/ct_train_1001_image.nii.gz',
         path: 'nifti/cardiac/ct_train_1002_image.nii.gz',
     }, 
 ]
