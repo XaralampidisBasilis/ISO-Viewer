858deb3c486ca070990be8d351e2ebca14a81b18
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2025-10-15 16:25:08 +0300

    update docs

--- src/shaders/iso_viewer/chunks/march/modules/march_cells/intersected_cell/intersected_cell_trilinear_bernstein.glsl
+++ src/shaders/iso_viewer/chunks/march/modules/march_cells/intersected_cell/intersected_cell_trilinear_bernstein.glsl
@@ -10,12 +10,14 @@ if (sign_change(cubic.bernstein_coeffs))
     // from the sampled intensities we can compute the trilinear interpolation cubic polynomial coefficients
     cubic.coeffs = cubic.residuals * cubic_inv_vander;
 
-    // check cubic intersection and sign crossings for degenerate cases 
-    #if VARIATION_ENABLED == 1
-    cell.intersected = sign_change(cubic.residuals) || poly3_has_root(cubic.coeffs, 0.0, 1.0);
-    #else
+    // check cubic intersection and sign crossings for degenerate cases
     cell.intersected = sign_change(cubic.residuals) || is_cubic_solvable(cubic.coeffs, sampling_points.xw, cubic.residuals.xw);
-    #endif
+ 
+    // #if VARIATION_ENABLED == 1
+    // cell.intersected = sign_change(cubic.residuals) || poly3_has_root(cubic.coeffs, 0.0, 1.0);
+    // #else
+    // cell.intersected = sign_change(cubic.residuals) || is_cubic_solvable(cubic.coeffs, sampling_points.xw, cubic.residuals.xw);
+    // #endif
 
     #if DEBUG_ENABLED == 1
     stats.num_intersection_tests += 1;
